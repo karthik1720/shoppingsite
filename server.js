17e512b390297e1c32b997ccde1cc01a3f52d4d6
karthik <karthik.cs1720@gmail.com>
2022-11-19 23:14:23 +0530

    cors

--- server.js
+++ server.js
@@ -5,20 +5,28 @@ import { router as authRoutes } from "./routes/authRoutes.js";
 import { router as productsRoutes } from "./routes/productsRoutes.js";
 import { router as userRoutes } from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+// import cors from "cors";
 const app = express();
 
 dotenv.config();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(
-  cors({
-    origin:
-      "https://6379121451bb0c000878a47f--unique-muffin-54d003.netlify.app/",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin:
+//       "https://6379121451bb0c000878a47f--unique-muffin-54d003.netlify.app/",
+//     credentials: true,
+//   })
+// );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productsRoutes);
 app.use("/api/user", userRoutes);
